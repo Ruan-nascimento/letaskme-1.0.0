aa827c01ed4ef2bbfc6a942c7ce9c18bf567fa95
Ruan-nascimento <154020857+Ruan-nascimento@users.noreply.github.com>
2024-02-29 11:48:55 -0300

    first commit

---
+++ src/services/firebase.ts
@@ -0,0 +1,19 @@
+import {initializeApp}  from 'firebase/app'
+
+import {initializeAuth} from 'firebase/auth'
+import {Database} from 'firebase/database'
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_SOTRAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
+
+const App = initializeApp(firebaseConfig)
+
+export const auth = initializeAuth
+export const database = Database
